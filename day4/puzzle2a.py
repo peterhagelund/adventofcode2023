66c14fe7b0142a2bd855bd4ab00e37b448910a5c
Peter Hagelund <peterhagelund@mac.com>
2023-12-04 13:20:00 -0500

    alternate, faster solution to problem 2

--- day4/puzzle2a.py
+++ day4/puzzle2a.py
@@ -1,17 +1,17 @@
 from re import compile
 
 
-def process_card(sum: int, index: int, cards: list[tuple[list[int], list[int]]]) -> int:
-    """Processes a single card.
+def process_card(sum: int, index: int, counts: list[int]) -> int:
+    """Processes the outcome of a single card.
 
     Parameters
     ----------
     sum : int
         The current sum.
     index : int
-        The current card (index, not card number).
-    cards : list[tuple[list[int], list[int]]]
-        The cards.
+        The current index (not card number).
+    counts : list[int]
+        The winning card counts.
 
     Returns
     -------
@@ -19,34 +19,32 @@ def process_card(sum: int, index: int, cards: list[tuple[list[int], list[int]]])
         The resulting sum.
     """
     sum += 1
-    card = cards[index]
-    winning_numbers = card[0]
-    my_numbers = card[1]
-    count = 0
-    for number in my_numbers:
-        if number in winning_numbers:
-            count += 1
+    count = counts[index]
     for i in range(1, count + 1):
         new_index = index + i
-        if new_index < len(cards):
-            sum = process_card(sum, new_index, cards)
+        if new_index < len(counts):
+            sum = process_card(sum, new_index, counts)
     return sum
 
 
 def main():
     """Application entry-point."""
     pattern = compile(r"\d{1,2}")
-    cards = []
+    counts = []
     with open("puzzle_input.txt", "rt") as f:
         for line in f:
             _, numbers = line[:-1].split(":")
             winning_numbers, my_numbers = numbers.split("|")
             winning_numbers = [int(n) for n in pattern.findall(winning_numbers)]
             my_numbers = [int(n) for n in pattern.findall(my_numbers)]
-            cards.append((winning_numbers, my_numbers))
+            count = 0
+            for number in my_numbers:
+                if number in winning_numbers:
+                    count += 1
+            counts.append(count)
     sum = 0
-    for index in range(len(cards)):
-        sum = process_card(sum, index, cards)
+    for index in range(len(counts)):
+        sum = process_card(sum, index, counts)
     print(f"sum = {sum}")
 
 
