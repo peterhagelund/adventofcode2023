40d343ec83e0aba638b12bd0fb29442f0639ec70
Peter Hagelund <peterhagelund@mac.com>
2023-12-09 14:19:50 -0500

    solution to problems in dart

---
+++ day9/puzzle1.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+/// Extrapolates the last value of the specified [List].
+int extrapolate(List<int> values) {
+  var sequences = <List<int>>[];
+  var sequence = values;
+  while (true) {
+    sequences.add(sequence);
+    if ((sequence.reduce(max) == 0) && (sequence.reduce(min) == 0)) {
+      break;
+    }
+    var differences = <int>[];
+    for (var i = 0; i < sequence.length - 1; i++) {
+      differences.add(sequence[i + 1] - sequence[i]);
+    }
+    sequence = differences;
+  }
+  int last = 0;
+  for (var i = sequences.length - 1; i >= 0; i--) {
+    sequence = sequences[i];
+    if (i == sequences.length - 1) {
+      sequence.add(last);
+    } else {
+      last = sequence.last + last;
+      sequence.add(last);
+    }
+  }
+  return last;
+}
+
+/// Application entry-point.
+void main() async {
+  final lines = utf8.decoder
+      .bind(File("puzzle_input.txt").openRead())
+      .transform(const LineSplitter());
+  int sum = 0;
+  await for (final line in lines) {
+    var values = <int>[];
+    for (final value in line.split(' ')) {
+      values.add(int.parse(value));
+    }
+    sum += extrapolate(values);
+  }
+  print('sum = $sum');
+}
---
+++ day9/puzzle2.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+/// Extrapolates the first value of the specified [List].
+int extrapolate(List<int> values) {
+  var sequences = <List<int>>[];
+  var sequence = values;
+  while (true) {
+    sequences.add(sequence);
+    if ((sequence.reduce(max) == 0) && (sequence.reduce(min) == 0)) {
+      break;
+    }
+    var differences = <int>[];
+    for (var i = sequence.length - 1; i > 0; i--) {
+      differences.insert(0, sequence[i] - sequence[i - 1]);
+    }
+    sequence = differences;
+  }
+  int first = 0;
+  for (var i = sequences.length - 1; i >= 0; i--) {
+    sequence = sequences[i];
+    if (i == sequences.length - 1) {
+      sequence.add(first);
+    } else {
+      first = sequence.first - first;
+      sequence.insert(0, first);
+    }
+  }
+  return first;
+}
+
+/// Application entry-point.
+void main() async {
+  final lines = utf8.decoder
+      .bind(File("puzzle_input.txt").openRead())
+      .transform(const LineSplitter());
+  int sum = 0;
+  await for (final line in lines) {
+    var values = <int>[];
+    for (final value in line.split(' ')) {
+      values.add(int.parse(value));
+    }
+    sum += extrapolate(values);
+  }
+  print('sum = $sum');
+}
