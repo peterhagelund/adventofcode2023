ed9c4b55435fa68a930826e780dad65593d8cd44
Peter Hagelund <peterhagelund@mac.com>
2023-12-10 16:39:52 -0500

    additional sample data and some cleanup

--- day10/puzzle2.py
+++ day10/puzzle2.py
@@ -25,6 +25,13 @@ def find_start(maze: list[str]) -> Optional[tuple[int, int]]:
 def calculate_outside_count(start: tuple[int, int], maze: list[str]) -> int:
     """Calculates the outside count.
 
+    Determine what pipe-character `S` replaces in order for algorithm to work.
+    Initially it can be any of the charcters, but as we see different neighbors (north, south, east, west)
+    we can whittle down the options to just one.
+
+    Toggle inside/outside flag on `|` and end-of-runs of `L` to `J`.
+    Don't toggle on runs of `F` to `7` because we're running horizontally at the bottom of the line.
+
     Parameters
     ----------
     start : tuple[int, int]
@@ -38,7 +45,7 @@ def calculate_outside_count(start: tuple[int, int], maze: list[str]) -> int:
         The count of steps to the farthest point.
     """
     height = len(maze)
-    width = len(maze[0])
+    width = len(maze[0])  # We already know all lines are the same length; otherwise this wouldn't work.
     queue = deque([start])
     moves: set[tuple[int, int]] = {start}
     actual_s = {"|", "-", "J", "L", "7", "F"}
@@ -74,19 +81,19 @@ def calculate_outside_count(start: tuple[int, int], maze: list[str]) -> int:
     for y in range(height):
         line = maze[y]
         outside = True
-        up = False
+        vertical = False
         for x in range(width):
             c = line[x]
             if c == "|":
                 outside = not outside
             elif c == "-":
-                pass
+                pass  # We're going horizontal
             elif c in "LF":
-                up = c == "L"
+                vertical = c == "L"
             elif c in "7J":
-                if c != ("J" if up else "7"):
+                if c != ("J" if vertical else "7"):  # Toggle at the end of this run (`J` if start was `L`; `7` if start was `F`)
                     outside = not outside
-                up = False
+                vertical = False
             elif c == ".":
                 pass
             if outside:
