b8d353b43c741d017995c8a2551fa919b9c5df67
Peter Hagelund <peterhagelund@mac.com>
2023-12-16 16:38:42 -0500

    improved solutions for day 16 puzzles

--- day16/puzzle1.py
+++ day16/puzzle1.py
@@ -11,111 +11,94 @@ class Direction(Enum):
     UP = 4
 
 
+def calculate_energized_tiles(contraption: list[list[str]], start: tuple[int, int, Direction]) -> int:
+    """Calculates energized tiles.
+
+    Parameters
+    ----------
+    contraption : list[list[str]]
+        The contraption.
+    start : tuple[int, int, Direction]
+        The starting point.
+
+    Returns
+    -------
+    int
+        The energized tile count.
+
+    Raises
+    ------
+    ValueError
+        If an invalid tile is encountered.
+    """
+    legs: list[tuple[int, int, Direction]] = []
+    energized: list[list[bool]] = [[False for _ in row] for row in contraption]
+    queue = deque([start])
+    height = len(contraption)
+    width = len(contraption[0])
+    while queue:
+        leg = queue.popleft()
+        if leg in legs:
+            continue
+        legs.append(leg)
+        (y, x, direction) = leg
+        while True:
+            energized[y][x] = True
+            tile = contraption[y][x]
+            if direction == Direction.RIGHT:
+                if tile in ".-":
+                    x += 1
+                    if x == width:
+                        break
+                else:
+                    if y > 0 and tile in "|/":
+                        queue.append((y - 1, x, Direction.UP))
+                    if y + 1 < height and tile in "|\\":
+                        queue.append((y + 1, x, Direction.DOWN))
+                    break
+            elif direction == Direction.DOWN:
+                if tile in ".|":
+                    y += 1
+                    if y == height:
+                        break
+                else:
+                    if x > 0 and tile in "-/":
+                        queue.append((y, x - 1, Direction.LEFT))
+                    if x + 1 < width and tile in "-\\":
+                        queue.append((y, x + 1, Direction.RIGHT))
+                    break
+            elif direction == Direction.LEFT:
+                if tile in ".-":
+                    x -= 1
+                    if x < 0:
+                        break
+                else:
+                    if y > 0 and tile in "|\\":
+                        queue.append((y - 1, x, Direction.UP))
+                    if y < height - 1 and tile in "|/":
+                        queue.append((y + 1, x, Direction.DOWN))
+                    break
+            else:  # Direction.UP
+                if tile in ".|":
+                    y -= 1
+                    if y < 0:
+                        break
+                else:
+                    if x > 0 and tile in "-\\":
+                        queue.append((y, x - 1, Direction.LEFT))
+                    if x + 1 < width and tile in "-/":
+                        queue.append((y, x + 1, Direction.RIGHT))
+                    break
+    return sum([sum([0 if not tile else 1 for tile in row]) for row in energized])
+
+
 def main():
     """Application entry-point."""
     contraption: list[list[str]] = []
     with open("puzzle_input.txt", "rt") as f:
         for line in f:
             contraption.append(list(line.strip()))
-    legs: list[tuple[int, int, Direction]] = []
-    energized: list[list[bool]] = []
-    for row in contraption:
-        energized.append([False for _ in row])
-    queue = deque([(0, 0, Direction.RIGHT)])
-    while queue:
-        leg: tuple[int, int, Direction] = queue.popleft()
-        if leg in legs:
-            continue
-        legs.append(leg)
-        y = leg[0]
-        x = leg[1]
-        direction = leg[2]
-        while True:
-            if contraption[y][x] == ".":
-                energized[y][x] = True
-                if direction == Direction.RIGHT:
-                    x += 1
-                    if x == len(contraption[y]):
-                        break
-                elif direction == Direction.LEFT:
-                    x -= 1
-                    if x < 0:
-                        break
-                elif direction == Direction.DOWN:
-                    y += 1
-                    if y == len(contraption):
-                        break
-                else:
-                    y -= 1
-                    if y < 0:
-                        break
-            elif contraption[y][x] == "|":
-                energized[y][x] = True
-                if direction == Direction.DOWN:
-                    y += 1
-                    if y == len(contraption):
-                        break
-                elif direction == Direction.UP:
-                    y -= 1
-                    if y == 0:
-                        break
-                else:
-                    if y > 0:
-                        queue.append((y - 1, x, Direction.UP))
-                    if y < len(contraption) - 1:
-                        queue.append((y + 1, x, Direction.DOWN))
-                    break
-            elif contraption[y][x] == "-":
-                energized[y][x] = True
-                if direction == Direction.RIGHT:
-                    x += 1
-                    if x == len(contraption[y]):
-                        break
-                elif direction == Direction.LEFT:
-                    x -= 1
-                    if x < 0:
-                        break
-                else:
-                    if x > 0:
-                        queue.append((y, x - 1, Direction.LEFT))
-                    if x < len(contraption[y]) - 1:
-                        queue.append((y, x + 1, Direction.RIGHT))
-                    break
-            elif contraption[y][x] == "/":
-                energized[y][x] = True
-                if direction == Direction.RIGHT:
-                    if y > 0:
-                        queue.append((y - 1, x, Direction.UP))
-                elif direction == Direction.LEFT:
-                    if y < len(contraption) - 1:
-                        queue.append((y + 1, x, Direction.DOWN))
-                elif direction == Direction.DOWN:
-                    if x > 0:
-                        queue.append((y, x - 1, Direction.LEFT))
-                else:
-                    if x < len(contraption[y]) - 1:
-                        queue.append((y, x + 1, Direction.RIGHT))
-                break
-            elif contraption[y][x] == "\\":
-                energized[y][x] = True
-                if direction == Direction.RIGHT:
-                    if y < len(contraption) - 1:
-                        queue.append((y + 1, x, Direction.DOWN))
-                elif direction == Direction.LEFT:
-                    if y > 0:
-                        queue.append((y - 1, x, Direction.UP))
-                elif direction == Direction.DOWN:
-                    if x < len(contraption[y]) - 1:
-                        queue.append((y, x + 1, Direction.RIGHT))
-                else:
-                    if x > 0:
-                        queue.append((y, x - 1, Direction.LEFT))
-                break
-            else:
-                raise ValueError(contraption[y][x])
-    tile_count = 0
-    for row in energized:
-        tile_count += sum([0 if not tile else 1 for tile in row])
+    tile_count = calculate_energized_tiles(contraption, (0, 0, Direction.RIGHT))
     print(f"energized tiles = {tile_count}")
 
 
