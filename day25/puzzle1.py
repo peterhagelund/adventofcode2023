bfa831a9fe63cdf385ea7817107649587632befd
Peter Hagelund <peterhagelund@mac.com>
2023-12-25 16:41:26 -0500

    solution to day 25 problem 1

---
+++ day25/puzzle1.py
@@ -0,0 +1,21 @@
+from networkx import Graph, connected_components, minimum_edge_cut
+
+
+def main():
+    """Application entry-point."""
+    graph = Graph()
+    with open("puzzle_input.txt", "rt") as f:
+        for line in f:
+            source, destinations = line.strip().split(":")
+            for destination in destinations.strip().split(" "):
+                graph.add_edge(source, destination)
+                graph.add_edge(destination, source)
+    graph.remove_edges_from(minimum_edge_cut(graph))
+    group1, group2 = connected_components(graph)
+    print(f"group 1: {len(group1)}")
+    print(f"group 2: {len(group2)}")
+    print(f"product = {len(group1) * len(group2)}")
+
+
+if __name__ == "__main__":
+    main()
