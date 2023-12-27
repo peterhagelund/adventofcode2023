d54845e7a477a970982787c2b463bb35ea43fd1e
Peter Hagelund <peterhagelund@mac.com>
2023-12-27 11:34:29 -0500

    fixes and cleanup

--- day25/puzzle1.py
+++ day25/puzzle1.py
@@ -9,7 +9,6 @@ def main():
             source, destinations = line.strip().split(":")
             for destination in destinations.strip().split(" "):
                 graph.add_edge(source, destination)
-                graph.add_edge(destination, source)
     graph.remove_edges_from(minimum_edge_cut(graph))
     group1, group2 = connected_components(graph)
     print(f"group 1: {len(group1)}")
