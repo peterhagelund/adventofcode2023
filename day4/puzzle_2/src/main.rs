5dd1dd2810f581c9c92cf59682312af30686ab30
Peter Hagelund <peterhagelund@mac.com>
2024-02-13 13:56:44 -0500

    use slices along with some simplifications

--- day4/puzzle_2/src/main.rs
+++ day4/puzzle_2/src/main.rs
@@ -2,15 +2,15 @@ use std::fs::read_to_string;
 
 fn main() {
     let s = read_to_string("../puzzle_input.txt").unwrap();
-    let mut counts: Vec<u32> = Vec::new();
+    let mut counts: Vec<usize> = Vec::new();
     for card in s.lines() {
         let parts: Vec<&str> = card.split(|c| ":|".contains(c)).collect();
-        let winning_numbers: Vec<u32> = parts[1]
+        let winning_numbers: Vec<usize> = parts[1]
             .split(|c| char::is_whitespace(c))
             .filter(|s| !s.is_empty())
             .map(|s| s.parse().unwrap())
             .collect();
-        let my_numbers: Vec<u32> = parts[2]
+        let my_numbers: Vec<usize> = parts[2]
             .split(|c| char::is_whitespace(c))
             .filter(|s| !s.is_empty())
             .map(|s| s.parse().unwrap())
@@ -25,7 +25,7 @@ fn main() {
     }
     let mut sum = 0;
     for index in 0..counts.len() {
-        sum = process_card(sum, index, &counts);
+        process_card(&mut sum, &counts[index..]);
     }
     println!("sum = {sum}");
 }
@@ -33,14 +33,9 @@ fn main() {
 /// Processes the outcome of a single card.
 ///
 /// The card is processed and then all subsequent cards are processed recursively.
-fn process_card(sum: u32, index: usize, counts: &Vec<u32>) -> u32 {
-    let mut sum = sum + 1;
-    let count = counts[index];
-    for i in 1..count + 1 {
-        let new_index = index + i as usize;
-        if new_index < counts.len() {
-            sum = process_card(sum, new_index, counts);
-        }
+fn process_card(sum: &mut u32, counts: &[usize]) {
+    *sum += 1;
+    for i in 1..=counts[0] {
+        process_card(sum, &counts[i..]);
     }
-    sum
 }
