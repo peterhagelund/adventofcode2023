5572c9467af4627e41b90696df5bf9faf2511b76
Peter Hagelund <peterhagelund@mac.com>
2024-03-03 13:23:01 -0500

    solutions to day 13 problems in go

---
+++ day13/cmd/puzzle2/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+)
+
+func main() {
+	file, err := os.Open("puzzle_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	patterns := make([][]rune, 0)
+	sum := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if len(text) > 0 {
+			patterns = append(patterns, []rune(text))
+		} else {
+			sum += processPatterns(patterns)
+			patterns = make([][]rune, 0)
+		}
+	}
+	if len(patterns) > 0 {
+		sum += processPatterns(patterns)
+	}
+	fmt.Printf("sum = %d\n", sum)
+}
+
+func processPatterns(patterns [][]rune) int {
+	result := 0
+	result += findReflection(patterns) * 100
+	flipped := make([][]rune, len(patterns[0]))
+	for x := 0; x < len(patterns[0]); x++ {
+		flipped[x] = make([]rune, len(patterns))
+		for y := 0; y < len(patterns); y++ {
+			flipped[x][y] = patterns[y][x]
+		}
+	}
+	result += findReflection(flipped)
+	return result
+}
+
+func findReflection(patterns [][]rune) int {
+	for row := 1; row < len(patterns); row++ {
+		above := slices.Clone(patterns[:row])
+		slices.Reverse(above)
+		below := slices.Clone(patterns[row:])
+		size := min(len(above), len(below))
+		diff := 0
+		for y := 0; y < size; y++ {
+			for x := 0; x < len(above[0]); x++ {
+				if above[y][x] != below[y][x] {
+					diff++
+				}
+			}
+		}
+		if diff == 1 {
+			return row
+		}
+	}
+	return 0
+}
